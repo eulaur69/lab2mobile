62df706939fa22732bfd9c5e140de77b0af7f946
eulaur69 <77895139+eulaur69@users.noreply.github.com>
2021-10-25 13:02:20 +0300

    Add files via upload

--- orientare.js
+++ orientare.js
@@ -1,16 +1,13 @@
 window.addEventListener("deviceorientation", on_device_orientation);
 
-function on_device_orientation(event) {
-    var alpha = event.alpha;
-    var beta = event.beta;
-    var gamma = event.gamma;
-    console.log(event.alpha);
-    console.log(event.beta);
-    console.log(event.gamma);
+function on_device_orientation(evt) {
+    var alpha = evt.alpha;
+    var beta = evt.beta;
+    var gamma = evt.gamma;
 
-    document.getElementById("a").innerHTML = "Alfa = " + alpha;
-    document.getElementById("b").innerHTML = "Beta = " + beta;
-    document.getElementById("c").innerHTML = "Gamma = " + gamma;
+    document.getElementById("a").innerHTML = "alpha = " + alpha;
+    document.getElementById("b").innerHTML = "beta = " + beta;
+    document.getElementById("c").innerHTML = "gamma = " + gamma;
 
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
